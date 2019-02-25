8ac7ddb0d349eb2c151c892c047cd360ad4b4bb8
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2019-02-25 11:01:39 +0200

    test

--- react/Task 8/index.js
+++ react/Task 8/index.js
@@ -15,3 +15,20 @@ const obj2 = {
         }
     ]
 };
+const obj1 = {
+    "list": [
+        {
+            "dt": 1487246400,
+            "main": {
+                "temp": 286.67,
+                "temp_min": 281.556,
+                "temp_max": 286.67,
+                "pressure": 972.73,
+                "sea_level": 1046.46,
+                "grnd_level": 972.73,
+                "humidity": 75,
+                "temp_kf": 5.11
+            }
+        }
+    ]
+};
