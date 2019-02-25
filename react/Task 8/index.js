db2dab6779e798bcb2631345bb633016d6041457
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2019-02-25 10:53:44 +0200

    test

---
+++ react/Task 8/index.js
@@ -0,0 +1,17 @@
+const obj2 = {
+    "list": [
+        {
+            "dt": 1487246400,
+            "main": {
+                "temp": 286.67,
+                "temp_min": 281.556,
+                "temp_max": 286.67,
+                "pressure": 972.73,
+                "sea_level": 1046.46,
+                "grnd_level": 972.73,
+                "humidity": 75,
+                "temp_kf": 5.11
+            }
+        }
+    ]
+};
