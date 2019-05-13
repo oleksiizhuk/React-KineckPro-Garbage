1485ad55196d936a18a931cdcaf42b0fd8ace15e
Oleksii Zhuk <oleksiizhuk.att@gmail.com>
2019-05-13 03:20:48 +0300

    add modal

--- verstka/js/index.js
+++ verstka/js/index.js
@@ -75,4 +75,25 @@ $(function () {
         slidesToScroll: 1
     });
 
+
+    /section online order/
+    const inputFile = $('.inputFile');
+
+    inputFile.change("click", function () {
+            let inputFileValue = inputFile.val();
+        if (!inputFileValue) {
+            return false;
+        } else {
+            let spanText = $(".section-9__download-block__text-div__download-text");
+            let spanDate = $(".section-9__download-block__item-date");
+            const downloadItem = $(".section-9__download-block__item");
+            downloadItem.removeClass("visibilityHidden");
+            spanText.text(inputFileValue);
+            const today = new Date();
+            const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            spanDate.text(date);
+        }
+
+    });
+
 });
